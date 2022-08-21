69dc1ae3656ba6beeb209b20904a000eea6883a2
Noah Marinic <moah.marinic@hotmail.de>
2022-08-21 13:10:08 +0200

    Changed docblock comments
    Put in year for every file, added reference to the university and the StuPro, removed E-Mail for now

--- jupyter/mod_form.php
+++ jupyter/mod_form.php
@@ -18,7 +18,7 @@
  * The main mod_jupyter configuration form.
  *
  * @package     mod_jupyter
- * @copyright   2022 Your Name <your@email.address>
+ * @copyright   2022 onwards, University of Stuttgart(StuPro 2022)
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -30,7 +30,7 @@ require_once($CFG->dirroot.'/course/moodleform_mod.php');
  * Module instance settings form.
  *
  * @package     mod_jupyter
- * @copyright   2022 Your Name <your@email.address>
+ * @copyright   2022 onwards, University of Stuttgart(StuPro 2022)
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class mod_jupyter_mod_form extends moodleform_mod {
